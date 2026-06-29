547a1d76042ab40e43ef2bb072aa95e50156e86e
bender <kudin.dima@gmail.com>
2026-06-29 20:26:34 +0000

    Switch to version 18: modified src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 
-export default function App) {
-  return 
+export default function App() {
+  return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<HomePage />} />
