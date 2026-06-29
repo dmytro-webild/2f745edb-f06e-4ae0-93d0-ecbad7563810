7f7f84fd4edc493ec73889f08e24a1006e8d152d
bender <kudin.dima@gmail.com>
2026-06-29 20:48:14 +0000

    Update src/pages/HomePage/sections/Contacto.tsx

--- src/pages/HomePage/sections/Contacto.tsx
+++ src/pages/HomePage/sections/Contacto.tsx
@@ -15,7 +15,7 @@ export default function ContactoSection(): React.JSX.Element {
           primaryButton={{
             text: "Solicitar presupuesto",        href: "mailto:contacto@ivandisenoweb.com"}}
           secondaryButton={{
-            text: "WhatsApp",        href: "https://wa.me/34600000000"}}
+            text: "WhatsApp",        href: ""}}
         />
         </SectionErrorBoundary>
       </div>
